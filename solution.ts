0140fe2e4b27b0cd0ec7bf68c96228cf5863330c
Abu Naim <abunaim5@users.noreply.github.com>
2025-11-17 20:16:56 +0600

    added solution for problem 2

--- solution.ts
+++ solution.ts
@@ -1,8 +1,12 @@
-type FormatValue = string | number | boolean;
-
-const formatValue = (value: FormatValue): FormatValue => {
+const formatValue = (value: string | number | boolean): string | number | boolean => {
     return typeof value === 'string' ? value.toUpperCase() :
         typeof value === 'number' ? value * 10 :
             typeof value === 'boolean' ? !value :
-                '';
+                'Unexpected value type';
+};
+
+const getLength = (value: string | any[]): number => {
+    return typeof value === 'string' ? value.length :
+        Array.isArray(value) ? value.length :
+            0;
 };
